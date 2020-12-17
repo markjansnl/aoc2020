a98fdc1e28bcdc5d524a3bad360559d84117daa7
Mark Jans <mark@markjans.nl>
2020-12-17 10:55:32 +0100

    Aoc15: array instead of hashmap

--- aoc15/src/lib.rs
+++ aoc15/src/lib.rs
@@ -1,14 +1,15 @@
-use std::collections::HashMap;
-
 pub fn spoken_number(starting_numbers: Vec<usize>, nth: usize) -> usize {
-    let mut map = HashMap::new();
+    let mut map = vec![0usize; nth];
     for (turn, &starting_number) in starting_numbers[0..starting_numbers.len() - 1].iter().enumerate() {
-        map.insert(starting_number, turn);
+        map[starting_number] = turn + 1;
     }
 
     let mut number = starting_numbers[starting_numbers.len() - 1];
-    for turn in map.len()..nth - 1 {
-        number = turn - map.insert(number, turn).unwrap_or(turn);
+    for turn in starting_numbers.len()..nth {
+        number = turn - match std::mem::replace(&mut map[number], turn) {
+            0               => turn,
+            old_value => old_value,
+        }
     }
 
     number
