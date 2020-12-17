e89033486791a3d86f8e61196c2443307ca240e6
Mark Jans <mark@markjans.nl>
2020-12-17 11:10:49 +0100

    Aoc15 code cleanups

--- aoc15/src/lib.rs
+++ aoc15/src/lib.rs
@@ -1,14 +1,14 @@
 pub fn spoken_number(starting_numbers: Vec<usize>, nth: usize) -> usize {
     let mut map = vec![0usize; nth];
-    for (turn, &starting_number) in starting_numbers[0..starting_numbers.len() - 1].iter().enumerate() {
-        map[starting_number] = turn + 1;
+    for turn in 1..starting_numbers.len() {
+        map[starting_numbers[turn - 1]] = turn;
     }
 
     let mut number = starting_numbers[starting_numbers.len() - 1];
     for turn in starting_numbers.len()..nth {
-        number = turn - match std::mem::replace(&mut map[number], turn) {
-            0               => turn,
-            old_value => old_value,
+        number = match std::mem::replace(&mut map[number], turn) {
+            0               => 0,
+            old_value => turn - old_value,
         }
     }
 
